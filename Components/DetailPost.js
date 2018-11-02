d6dbc3e4d7c84efbc280187866c7a60875764a2f
bob <osm147.1>
2018-11-03 02:15:02 +0300

    font familiy add

--- Components/DetailPost.js
+++ Components/DetailPost.js
@@ -1,6 +1,12 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView
+} from 'react-native';
 import {
   StackNavigator
 } from 'react-navigation';
@@ -10,26 +16,58 @@ import {
 
 
  class DetailPost extends Component{
-
+static navigationOptions = {
+     title: 'Esmaül Hüsna',
+     headerStyle: {
+       backgroundColor: '#5CB3C7',
+     },
+     headerTintColor: '#fff',
+     headerTitleStyle: {
+       fontWeight: 'bold',
+     },
+   };
   
 
 
   render() {
     const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId');
-    const isim = navigation.getParam('isim')
+    const isim = navigation.getParam('isim');
+    const arapca = navigation.getParam('arapca');
+    const aciklama = navigation.getParam('aciklama');
+    
+    
     
     return (
+      <ScrollView>
       <View style={styles.container}>
           <View>
             <View style={styles.kutu} >
-                <Text>{itemId}</Text>
-                <Text> {isim} </Text>
-                  <Text>asdasd</Text>
-                  <Text>asdasd</Text>
+                 
+              <View style={{marginBottom:20}} >
+                  <View style={styles.ynn}>
+                      <View><Text style={styles.baslik1}>Türkçe:</Text></View>
+                      <View><Text style={styles.yazi} >{isim}</Text></View>
+                  </View>
+
+                  <View style={styles.ynn}>
+                      <View><Text style={styles.baslik1}>Arapca:</Text></View>
+                      <View><Text style={styles.yazi}>{arapca}</Text></View>
+                  </View>
+                
+                  <View style={{justifyContent:'space-between',margin:5}}>
+                      <View style={{marginBottom:5}}><Text style={styles.baslik1}>Aciklama:</Text></View>
+                      <View><Text style={styles.yazi}>{aciklama}</Text></View>
+                  </View>
+                
+              </View>
+                
+                
+                
+                
             </View>
           </View>
       </View>
+      </ScrollView>
     );
   }
 }
@@ -37,11 +75,28 @@ import {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#f5f5f5',
   },
   kutu:{
-    backgroundColor:'green',
+    
+    backgroundColor:'#fff',
     margin: 10,
+    borderWidth: 4,
+    borderColor:'#fff'
+    
+  },
+  yazi:{
+    fontSize:16
+  },
+  ynn:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    margin:5
+  },
+  baslik1:{
+    fontSize:14,
+    fontFamily:'alnile'
+
   }
 
 });
--- Navigator/Pages/Home/index.js
+++ Navigator/Pages/Home/index.js
@@ -30,8 +30,7 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  
     backgroundColor: '#F5FCFF',
   },
 
